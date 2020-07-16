30ddafd7f8e81afaae69519736e0846f5e046524
damian <damian@casa.com>
2020-07-15 21:06:16 -0300

    modificación desde consola

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("D'Arrigo");
+	alert("Esto funciona prefecto");
 }
 
