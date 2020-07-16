cc2c3b91536d79c3502c92fa3461d4410622547a
damian <damian@casa.com>
2020-07-15 21:14:30 -0300

    Arreglo

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -6,14 +6,3 @@ function mostrar()
 {
 	alert("Esto funciona de maravilla");
 }
-
-=======
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("Esto funciona de maravilla");
-}
-
->>>>>>> 35052bba399161231adb36c3ba086feb90174477
