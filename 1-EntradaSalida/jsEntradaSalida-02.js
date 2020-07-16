9f1fe666acf544340b24c13886221df61c4cbbd3
Damián D'Arrigo <ddarrigo.87@gmail.com>
2020-07-16 20:44:20 -0300

    Camnbios día 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,13 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	// Declarar una variable
+	var vNombre;
+	// Asignación
+	vNombre= prompt("Nombre","Ingrese un nombre"); // asignando prompt a vNombre
+
+	alert(vNombre);
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,8 +3,13 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
+
+	// Declarar una variable
+	var ID;
+	// Asignación
+	vNombre= prompt("Nombre","Ingrese un nombre"); 
+
+	nombreIngresado=txtIdNombre.value; // asignando ID a vNombre
 	alert("ok");
 
 }
