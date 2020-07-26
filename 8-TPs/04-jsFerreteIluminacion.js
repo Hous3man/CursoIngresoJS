1ee0fe14d599a02d2c6e652381cd730d9fdd7f4b
damian <damian@casa.com>
2020-07-26 10:48:23 -0300

    TP4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -4,7 +4,7 @@ A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos eh informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
@@ -14,62 +14,63 @@ function CalcularPrecio ()
     let vLamp;
     let vMarca;
     let vPrecio;
-    let vImpuesto;
+    let vImp;
 
     // Tomar valores de campos
     vLamp=parseInt(document.getElementById("txtIdCantidad").value);
     vMarca=document.getElementById("Marca").value;
-    vImpuesto=(document.getElementById("txtIdprecioDescuento").value)*1.10
 
-    if (vLamp>=6)
+    // Precio estándar de lámparas
+    vPrecio=vLamp*35;
+
+    // Calcular variantes de descuento
+    if(vLamp>=6)
     {
-        vPrecio=(vLamp*35-(vLamp*35*0.5));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio;
+        vPrecio=vPrecio*0.5;
     }
-    if(vLamp==5 && vMarca=="ArgentinaLuz")
+    else if(vLamp==5)
     {
-        vPrecio=(vLamp*35-(vLamp*35*0.4));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
+        switch(vMarca)
+        {
+        case "ArgentinaLuz":
+        vPrecio=vPrecio*0.6;
+        break;
+        default:
+        vPrecio=vPrecio*0.7
+        break;
+        }
     }    
-    else if (vLamp==5 && vMarca!=="ArgentinaLuz")
-    {        
-        vPrecio=(vLamp*35-(vLamp*35*0.3));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
-    }
-    if(vLamp==4 && vMarca=="ArgentinaLuz" || vMarca=="FelipeLamparas")
+    else if(vLamp==4)
     {
-        vPrecio=(vLamp*35-(vLamp*35*0.25));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
+        switch(vMarca)
+        {
+            case "ArgentinaLuz":
+            case "FelipeLamparas":
+            vPrecio=vPrecio*0.75;
+            break;
+            default:
+            vPrecio=vPrecio*0.8;
+            break;
+        }
     }
-    else if(vLamp==4 && vMarca!="ArgentinaLuz" || vMarca!="FelipeLamparas")
-    {
-            vPrecio=(vLamp*35-(vLamp*35*0.2));
-            document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
-    }
-    if(vLamp==3 && vMarca=="ArgentinaLuz")
-    {
-        vPrecio=(vLamp*35-(vLamp*35*0.15));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
-    }
-    else if(vLamp==3 && vMarca=="FelipeLamparas")
-    {
-        vPrecio=(vLamp*35-(vLamp*35*0.1));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
-    }
-    else if(vLamp==3 && vMarca!="ArgentinaLuz" || vMarca!="FelipeLamparas")
-    {
-        vPrecio=(vLamp*35-(vLamp*35*0.05));
-        document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
-    }
-    if(vLamp<3)
-    {
-        vPrecio=vLamp*35;
-        document.getElementById("txtIdprecioDescuento").value=vPrecio;
-    }
+    else if(vLamp==3)
+        switch(vMarca)
+        {
+            case "ArgentinaLuz":
+            vPrecio=vPrecio*0.85;
+            break;
+            case "FelipeLamparas":
+            vPrecio=vPrecio*0.9;
+            default:
+            vPrecio=vPrecio*0.95;
+        }
+    /// Calcular impuesto y mostrarlo
     if(vPrecio>120)
     {
-        document.getElementById("txtIdprecioDescuento").value=vImpuesto;
-        alert("IIBB Usted pago "+vImpuesto)
+        vPrecio=vPrecio*1.1
+        vImp=vPrecio*0.1
+        alert("IIBB. Usted pago $"+vImp.toFixed(2));
     }
+    // Pone el valor del precio en el campo "Precio con descuento"
+    document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
 }
-
